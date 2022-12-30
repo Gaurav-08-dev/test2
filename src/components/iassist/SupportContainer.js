0a98b58feba64653976bc08ef32df02dcecf6062
Thanveer Ahamed A <thanveer@ascendeum.com>
2022-12-30 16:45:17 +0530

    push

--- src/components/iassist/SupportContainer.js
+++ src/components/iassist/SupportContainer.js
@@ -110,7 +110,7 @@ const SupportContainer = () => {
 
 
        console.log(document.getElementById(btnId))
-       document.getElementById(btnId).addEventListener('click', supportButtonClick)
+       document.getElementById(btnId)?.addEventListener('click', supportButtonClick)
 
     },[])
 
