34caa31c4b1feeb9ff57ccf1279de4ef000a9c85
gaurav-08-dev <g8111997@gmail.com>
2023-01-04 17:55:59 +0530

    changes

--- src/components/iassist/SupportContainer.js
+++ src/components/iassist/SupportContainer.js
@@ -99,7 +99,7 @@ const SupportContainer = () => {
         const bodyElement = document.getElementsByTagName('body')[0];
 
         const linkTag = document.createElement("link")
-        linkTag.href = 'https://gaurav-08-dev.github.io/test2/index.css';
+        linkTag.href = 'https://iassist-assets.s3.us-east-2.amazonaws.com/css/iassist.css';
         // linkTag.href = 'https://iassist-dev.bydata.com/scripts/sight/docs/index.css';
         linkTag.rel = "stylesheet";
         linkTag.id = "iassist-css";
