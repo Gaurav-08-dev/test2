1c0d9ce891a340dfb34ae8411307fb347d01bb80
Thanveer Ahamed A <thanveer@ascendeum.com>
2023-01-19 17:56:11 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+const bodyElement = document.getElementsByTagName('body')[0];
 const div = document.createElement('div')
         div.id = 'iassist-panel-wrapper';
         bodyElement.append(div);
