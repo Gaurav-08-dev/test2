f3a34a5290e108985a406eeeef7da4dc91a74075
Thanveer Ahamed A <thanveer@ascendeum.com>
2023-01-30 16:50:15 +0530

    remove unwanted lines

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-const bodyElement = document.getElementsByTagName('body')[0];
-const div = document.createElement('div')
-        div.id = 'iassist-panel-wrapper';
-        bodyElement.append(div);
+// const bodyElement = document.getElementsByTagName('body')[0];
+// const div = document.createElement('div')
+//         div.id = 'iassist-panel-wrapper';
+//         bodyElement.append(div);
 
 const root = ReactDOM.createRoot(document.getElementById('iassist-panel-wrapper'));
 root.render(
