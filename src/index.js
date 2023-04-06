5f7f37f8c647fe3b2f8d9c88430a87782d524c45
Thanveer Ahamed A <thanveer@ascendeum.com>
2023-04-06 10:09:33 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
-import disableReactDevTools from "./utils/DisableDevTools"
+// import disableReactDevTools from "./utils/DisableDevTools"
 
 
 // if (process.env.NODE_ENV === "production") disableReactDevTools();
