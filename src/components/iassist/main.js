f12b83befc883abcb2d539a37ce5a014a90a5f04
Thanveer Ahamed A <thanveer@ascendeum.com>
2023-05-11 11:44:39 +0530

    update

--- src/components/iassist/main.js
+++ src/components/iassist/main.js
@@ -19,25 +19,28 @@ const Main = () => {
         if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
             // return true;
             setIsElectronApp(true)
+            return;
         }
 
         // Main process
         if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
             // return true;
             setIsElectronApp(true)
-
+            return
         }
 
         // Detect the user agent when the `nodeIntegration` option is set to true
         if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
             // return true;
             setIsElectronApp(true)
+            return
 
         }
         console.log('not electron')
         // return false;
         setIsAuthenticationSuccess(true)
         setIsElectronApp(false)
+        return;
 
     }
 
